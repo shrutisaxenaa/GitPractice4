db1a5586ad4fa50d3a0a4ac450d69d6dd0774b6c
shrutisaxenaa <106474924+shrutisaxenaa@users.noreply.github.com>
2022-07-20 16:36:26 -0500

    commitByShruti

--- src/main/java/gitPractice4/GitPractice4.java
+++ src/main/java/gitPractice4/GitPractice4.java
@@ -5,11 +5,15 @@ public class GitPractice4 {
 	public static void main(String[] args) {
 		 
 		System.out.println(sum(6,7));
+		System.out.println(multi(4,6));
 	}
 	
 	public static int sum(int a,int b) {
 		return a+b;
 		
+	}
+	public static int multi(int a, int b) {
+		return a*b;
 	}
 
 }
